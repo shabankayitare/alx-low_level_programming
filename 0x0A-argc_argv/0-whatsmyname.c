2ef7fd1a9804fee45558e67e6da4edd71b445f45
shabankayitare <shabankayitare@gmail.com>
2023-04-10 07:36:06 -0700

    1-args.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include "main.h"
 
-/* int main prints the name of the program
+/*
+ * int main prints the name of the program
  * argc is number of arguments
  * argv is array of arguments
  * return always 0 = True value
- */
+*/
 int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
 	{
-		int i;
-		for (i = 0; i < argc; i++)
-		{
-		printf("%s\n", argv[i]);
-		}
-		return (0);
+	printf("%s\n", argv[i]);
 	}
+	return (0);
+}
